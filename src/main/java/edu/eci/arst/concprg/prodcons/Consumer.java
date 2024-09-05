c62a2f2c7e2c06e02f35c11e1de6e13608d4dd8d
ccastano46 <castanocamilo522@gmail.com>
2024-09-05 09:59:39 -0500

    Tratando de usar mas eficientemente la CPU

--- src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -6,6 +6,8 @@
 package edu.eci.arst.concprg.prodcons;
 
 import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,11 +25,23 @@ public class Consumer extends Thread{
     @Override
     public void run() {
         while (true) {
-
-            if (queue.size() > 0) {
-                int elem=queue.poll();
-                System.out.println("Consumer consumes "+elem);                                
+            try {
+                Thread.sleep(5000);
+                if (queue.size() > 0) {
+                    int elem=queue.poll();
+                    System.out.println("Consumer consumes "+elem);
+                }else{
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException ex) {
+                        Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Consumer.class.getName()).log(Level.SEVERE, null, ex);
             }
+
+
             
         }
     }
